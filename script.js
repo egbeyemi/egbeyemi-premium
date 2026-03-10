fe287c0eaa7fd3044a46180013a11ec09f86a744
Mr-Anonymous <abdulrahmonegbeyemi@gmail.com>
2026-03-10 05:26:29 +0100

    Add functions to open form and show payment message

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function openForm(){
+document.getElementById("formSection").style.display="block";
+}
 
+function showPayment(){
+
+let username = document.getElementById("username").value;
+let whatsapp = document.getElementById("whatsapp").value;
+let plan = document.getElementById("plan").value;
+
+let message = "Hello Egbeyemi Premium,%0A%0AI want Spotify Premium.%0A%0ASpotify Username: " 
++ username + "%0AMy WhatsApp: " + whatsapp + "%0APlan: " + plan + "%0A%0AI will send payment screenshot shortly.";
+
+let url = "https://wa.me/2349139103648?text=" + message;
+
+window.open(url, "_blank");
+
+}
